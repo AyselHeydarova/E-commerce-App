4d6a3b2afd6d091e0fd52ce3e59b26fec445c2b0
AysekHeydarova <aysel.esedova1994@gmail.com>
2020-06-17 00:23:44 +0400

    rating page created

--- App.js
+++ App.js
@@ -3,17 +3,10 @@ import {AppLoading} from "expo";
 import {StyleSheet, Text, View} from 'react-native';
 import {COLORS} from "./style/colors";
 
-import { loadFonts } from "./style/fonts";
-import {Input} from "./components/Input";
-import {Btn} from "./components/Btn";
-import {Category} from "./components/Category";
-import {IconFacebook} from "./components/IconFacebook";
-import {SocialBtn} from "./components/SocialBtn";
-import {SizeContainer} from "./components/SizeContainer";
-import { ProductCard } from "./components/ProductCard";
 import {loadFonts} from "./style/fonts";
-import {Filters} from "./commons/filters";
-import {IconContainer} from "./components/IconContainer";
+import { RatingReviews } from "./Screens/RatingReviews";
+
+
 
 export default function App() {
     const [loaded, setLoaded] = useState(false);
@@ -25,7 +18,9 @@ export default function App() {
     return (
 
            <View style={styles.container}>
-               <IconContainer/>
+               {/* <IconContainer/> */}
+               <RatingReviews/>
+               
            </View>
     );
 }
@@ -33,7 +28,7 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: COLORS.white,
+        backgroundColor: COLORS.BACKGROUND,
         alignItems: 'center',
         justifyContent: 'center',
     },
