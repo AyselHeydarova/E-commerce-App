088f9f4ed870d59c06d4fe359cc93c757d8cc62f
Elvin Ziyali <elvin.ziyali@gmail.com>
2020-06-26 18:22:22 +0400

    data handling

--- Screens/CategoriesOf.js
+++ Screens/CategoriesOf.js
@@ -5,67 +5,80 @@ import {Back} from "../Icons/Back";
 import {COLORS} from "../style/colors";
 import {Btn} from "../components/Btn";
 import {data} from "../DummyData/data";
+import {getAllData, getAllProductData} from "../store/products";
+import {connect} from "react-redux";
+import store from "../store";
 
+const mapStateToProps = (state) => ({
+    allProducts: getAllProductData(state),
+});
+export const CategoriesOf = connect(mapStateToProps, {getAllData})(
+    ({getAllData, allProducts, route, navigation}) => {
+        const {isWomanClicked} = route.params;
 
-export const CategoriesOf = ({navigation, route}) => {
-    const {isWomanClicked} = route.params;
+        const categoriesMan = [];
+        // const allCategories = Object.keys(data.categories);
 
-    const categoriesMan = [];
-    const allCategories = Object.keys(data.categories);
+        const everything = store.getState();
+        const allCategories = everything.products.categories;
+        const allCategoryNames = Object.keys(allCategories);
 
-    const checkMen = () => {
-        for (let category of allCategories) {
-            if (data.categories[`${category}`].men !== undefined) {
-                categoriesMan.push(category);
+        console.log("everything", everything.products.categories)
+
+        const checkMen = () => {
+            for (let category of allCategoryNames) {
+                if (allCategories[`${category}`].men !== undefined) {
+                    categoriesMan.push(category);
+                }
             }
-        }
 
-    };
-    useEffect(() => {
-        checkMen();
-    });
+        };
+        useEffect(() => {
+            checkMen();
+        });
 
-    return (
-        <View style={styles.container}>
-            <StatusBar/>
-            <View style={styles.header}>
-                <TouchableOpacity style={styles.backIcon} onPress={() => navigation.goBack()}>
-                    <Back/>
-                </TouchableOpacity>
-                <CustomText weight={'bold'} style={styles.title}>
-                    Categories
-                </CustomText>
-            </View>
-            <Btn
-                height={50}
-                width={335}
-                bgColor={COLORS.PRIMARY}
-                btnName={"VIEW ALL ITEMS"}
-                titleStyle={{fontSize: 18}}
-            />
-            <CustomText weight={'bold'} style={styles.choose}>
-                Choose Category
-            </CustomText>
-            <View style={{marginTop: 60}}>
-                <FlatList
-                    data={isWomanClicked ? allCategories : categoriesMan}
-                    // data={allCategories}
-                    renderItem={({item}) => (
-                        <TouchableOpacity style={styles.category}>
-                            <CustomText style={styles.categoryText}>
-                                {item}
-                            </CustomText>
-                        </TouchableOpacity>
-
-                    )}
-                    keyExtractor={item => item}
+        return (
+            <View style={styles.container}>
+                <StatusBar/>
+                <View style={styles.header}>
+                    <TouchableOpacity style={styles.backIcon} onPress={() => navigation.goBack()}>
+                        <Back/>
+                    </TouchableOpacity>
+                    <CustomText weight={'bold'} style={styles.title}>
+                        Categories
+                    </CustomText>
+                </View>
+                <Btn
+                    height={50}
+                    width={335}
+                    bgColor={COLORS.PRIMARY}
+                    btnName={"VIEW ALL ITEMS"}
+                    titleStyle={{fontSize: 18}}
                 />
+                <CustomText weight={'bold'} style={styles.choose}>
+                    Choose Category
+                </CustomText>
+                <View style={{marginTop: 60}}>
+                    <FlatList
+                        data={isWomanClicked ? allCategoryNames : categoriesMan}
+                        // data={allCategories}
+                        renderItem={({item}) => (
+                            <TouchableOpacity style={styles.category}
+                                              onPress={()=>navigation.navigate("")}>
+                                <CustomText style={styles.categoryText}>
+                                    {item}
+                                </CustomText>
+                            </TouchableOpacity>
+
+                        )}
+                        keyExtractor={item => item}
+                    />
+                </View>
+
+
             </View>
-
-
-        </View>
-    );
-};
+        );
+    });
 
 const styles = StyleSheet.create({
 
